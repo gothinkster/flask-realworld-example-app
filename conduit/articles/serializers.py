3bdf19869056b34fe485c721a94533824026a3e7
mohamed-aziz <medazizknani@gmail.com>
2017-05-05 22:39:26 +0100

    Added articles serializers and test.

---
+++ conduit/articles/serializers.py
@@ -0,0 +1,48 @@
+from marshmallow import Schema, fields, pre_load, post_dump, pre_dump
+from conduit.profile.serializers import ProfileSchema
+
+
+class TagSchema(Schema):
+    tagname = fields.Str()
+
+
+class ArticleSchema(Schema):
+    slug = fields.Str()
+    title = fields.Str()
+    description = fields.Str()
+    createdAt = fields.DateTime()
+    body = fields.Str()
+    updatedAt = fields.DateTime()
+    author = fields.Nested(ProfileSchema)
+    article = fields.Nested('self', exclude=('article',), default=True, load_only=True)
+    tagList = fields.List(fields.Str())
+    favoritesCount = fields.Int(dump_only=True)
+    favorited = fields.Bool(dump_only=True)
+
+    @pre_load
+    def make_article(self, data):
+        return data['article']
+
+    @post_dump
+    def dump_article(self, data):
+        data['author'] = data['author']['profile']
+        return {'article': data}
+
+    class Meta:
+        strict = True
+
+
+class ArticleSchemas(ArticleSchema):
+
+    @post_dump
+    def dump_article(self, data):
+        data['author'] = data['author']['profile']
+        return data
+
+    @post_dump(pass_many=True)
+    def dump_articles(self, data, many):
+        return {'articles': data, 'articlesCount': len(data)}
+
+
+article_schema = ArticleSchema()
+articles_schema = ArticleSchemas(many=True)
---
+++ tests/test_articles.py
@@ -0,0 +1,96 @@
+# coding: utf-8
+
+from flask import url_for
+
+
+class TestArticleViews:
+
+    def test_get_articles_by_author(self, testapp, user):
+        user = user.get()
+        resp = testapp.post_json(url_for('user.login_user'), {'user': {
+            'email': user.email,
+            'password': 'myprecious'
+        }})
+
+        token = resp.json['user']['token']
+        for _ in range(2):
+            testapp.post_json(url_for('articles.make_article'), {
+                "article": {
+                    "title": "How to train your dragon %i" % _,
+                    "description": "Ever wonder how?",
+                    "body": "You have to believe",
+                    "tagList": ["reactjs", "angularjs", "dragons"]
+                }
+            }, headers={
+                'Authorization': 'Token %s' % token
+            })
+
+        resp = testapp.get(url_for('articles.get_articles', author=user.username))
+        assert len(resp.json['articles']) == 2
+
+    def test_favorite_an_article(self, testapp, user):
+        user = user.get()
+        resp = testapp.post_json(url_for('user.login_user'), {'user': {
+            'email': user.email,
+            'password': 'myprecious'
+        }})
+
+        token = resp.json['user']['token']
+        resp1 = testapp.post_json(url_for('articles.make_article'), {
+            "article": {
+                "title": "How to train your dragon",
+                "description": "Ever wonder how?",
+                "body": "You have to believe",
+                "tagList": ["reactjs", "angularjs", "dragons"]
+            }
+        }, headers={
+            'Authorization': 'Token %s' % token
+        })
+
+        resp = testapp.post(url_for('articles.favorite_an_article',
+                                    slug=resp1.json['article']['slug']), headers={'Authorization': 'Token %s' % token})
+        assert resp.json['article']['favorited'] is True
+
+    def test_get_articles_by_favoriter(self, testapp, user):
+        user = user.get()
+        resp = testapp.post_json(url_for('user.login_user'), {'user': {
+            'email': user.email,
+            'password': 'myprecious'
+        }})
+
+        token = resp.json['user']['token']
+        for _ in range(2):
+            testapp.post_json(url_for('articles.make_article'), {
+                "article": {
+                    "title": "How to train your dragon %i" % _,
+                    "description": "Ever wonder how?",
+                    "body": "You have to believe",
+                    "tagList": ["reactjs", "angularjs", "dragons"]
+                }
+            }, headers={
+                'Authorization': 'Token %s' % token
+            })
+
+        resp = testapp.get(url_for('articles.get_articles', author=user.username))
+        assert len(resp.json['articles']) == 2
+
+    def test_make_article(self, testapp, user):
+        user = user.get()
+        resp = testapp.post_json(url_for('user.login_user'), {'user': {
+            'email': user.email,
+            'password': 'myprecious'
+        }})
+
+        token = resp.json['user']['token']
+        resp = testapp.post_json(url_for('articles.make_article'), {
+            "article": {
+                "title": "How to train your dragon",
+                "description": "Ever wonder how?",
+                "body": "You have to believe",
+                "tagList": ["reactjs", "angularjs", "dragons"]
+            }
+        }, headers={
+            'Authorization': 'Token %s' % token
+        })
+        assert resp.json['article']['author']['email'] == user.email
+        assert resp.json['article']['body'] == 'You have to believe'

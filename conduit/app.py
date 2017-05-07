4a7ec6e8172cd1a5d2801a29f9e2c575cab32d2f
mohamed-aziz <medazizknani@gmail.com>
2017-05-07 10:57:48 +0100

    Allow strict slashes.

--- conduit/app.py
+++ conduit/app.py
@@ -14,6 +14,7 @@ def create_app(config_object=ProdConfig):
     :param config_object: The configuration object to use.
     """
     app = Flask(__name__.split('.')[0])
+    app.url_map.strict_slashes = False
     app.config.from_object(config_object)
     register_extensions(app)
     register_blueprints(app)

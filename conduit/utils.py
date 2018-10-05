6665b9ea7592318136579d171942c909eee84baa
jerry <khanh96le@gmail.com>
2018-10-06 01:00:47 +0700

    refactor(*): rearrange code

--- conduit/utils.py
+++ conduit/utils.py
@@ -2,8 +2,10 @@
 """Helper utilities and decorators."""
 from conduit.user.models import User  # noqa
 
+
 def jwt_identity(payload):
     return User.get_by_id(payload)
+
 
 def identity_loader(user):
     return user.id

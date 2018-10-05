30147e0ded474cd5d91e8080180020bc64aa8573
jerry <khanh96le@gmail.com>
2018-10-06 00:53:42 +0700

    refactor(*): optimize imports

--- conduit/profile/views.py
+++ conduit/profile/views.py
@@ -4,10 +4,9 @@ from flask import Blueprint
 from flask_apispec import marshal_with
 from flask_jwt_extended import current_user, jwt_required, jwt_optional
 
-from .serializers import profile_schema
 from conduit.exceptions import InvalidUsage
 from conduit.user.models import User
-
+from .serializers import profile_schema
 
 blueprint = Blueprint('profiles', __name__)
 
